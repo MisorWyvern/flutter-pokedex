6635e948ba5d49741b8790aa7a7929d173f66b15
MisorWyvern <gabriel_rosim@yahoo.com.br>
2021-02-22 14:42:21 -0300

    add mapper and an empty theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_pokedex/core/themes/main_theme.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: mainTheme,
       initialRoute: "/",
       navigatorKey: Modular.navigatorKey,
       onGenerateRoute: Modular.generateRoute,
