606b77e5188c79e2de29fb14c1355304419099a7
MisorWyvern <gabriel_rosim@yahoo.com.br>
2021-02-26 12:44:24 -0300

    fix pokeball bg image positioning

--- lib/features/pokemon_list/presenter/pages/pokemon_list_page.dart
+++ lib/features/pokemon_list/presenter/pages/pokemon_list_page.dart
@@ -18,123 +18,131 @@ class _PokemonListPageState
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).backgroundColor,
-        image: DecorationImage(
-          image: AssetImage("assets/pokeball10.png"),
-          alignment: Alignment(2.25, -1.25),
-          fit: BoxFit.none,
-          scale: 1,
-        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(
-          top: 2 * 8.0,
-          left: 2 * 8.0,
-          right: 2 * 8.0,
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            elevation: 0.0,
-            backgroundColor: Colors.transparent,
-            actions: [
-              IconButton(
-                  icon: Icon(
-                    Icons.list,
-                    color: Colors.black87,
-                  ),
-                  onPressed: () {})
-            ],
+      child: Stack(
+        children: [
+          Positioned(
+            top: -65,
+            right: -85,
+            child: Image.asset(
+              "assets/pokeball10.png",
+              fit: BoxFit.none,
+            ),
           ),
-          body: SingleChildScrollView(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height:
-                  MediaQuery.of(context).size.height - kToolbarHeight - 8 * 5.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: LightSecondaryTitle(title: "Pokedex"),
-                  ),
-                  FutureBuilder(
-                    initialData: [],
-                    future: controller.updatePokemonList(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState != ConnectionState.done) {
-                        return Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              DarkSmallTitle(
-                                title: "Loading...",
-                                isTextAlignCenter: true,
-                              ),
-                              CircularProgressIndicator(),
-                            ],
-                          ),
-                        );
-                      }
-
-                      if (controller.pokemonList.length == 0) {
-                        return Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.mood_bad_outlined),
-                              DarkSmallTitle(
-                                title: "Something went wrong...",
-                                isTextAlignCenter: true,
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: Text(
-                                  "Verify your connection and press \"Try again\" button...",
-                                ),
-                              ),
-                              RaisedButton(
-                                onPressed: controller.updatePokemonList,
-                                child: Text("Try again"),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
-
-                      return Expanded(
-                        child: StaggeredGridView.extentBuilder(
-                          maxCrossAxisExtent: 180,
-                          mainAxisSpacing: 8,
-                          crossAxisSpacing: 8,
-                          itemBuilder: (_, index) {
-                            return PokemonTile(
-                              backgroundColor: controller.getColorByType(
-                                  controller.pokemonList[index].type[0]),
-                              pokemon: controller.pokemonList[index],
-                            );
-                          },
-                          staggeredTileBuilder: (i) => StaggeredTile.fit(1),
-                        ),
-                        // child: GridView.builder(
-                        //   itemCount: controller.pokemonList?.length ?? 0,
-                        //   gridDelegate:
-                        //       SliverGridDelegateWithMaxCrossAxisExtent(
-                        //     mainAxisSpacing: 8,
-                        //     crossAxisSpacing: 8,
-                        //     maxCrossAxisExtent: 180,
-                        //   ),
-                        //   itemBuilder:
-                        //   },
-                        // ),
-                      );
-                    },
-                  ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 2 * 8.0,
+              left: 2 * 8.0,
+              right: 2 * 8.0,
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                elevation: 0.0,
+                backgroundColor: Colors.transparent,
+                actions: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.list,
+                        color: Colors.black87,
+                      ),
+                      onPressed: () {}),
                 ],
+              ),
+              body: SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height -
+                      kToolbarHeight -
+                      8 * 5.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: LightSecondaryTitle(title: "Pokedex"),
+                      ),
+                      FutureBuilder(
+                        initialData: [],
+                        future: controller.updatePokemonList(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState !=
+                              ConnectionState.done) {
+                            return Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  DarkSmallTitle(
+                                    title: "Loading...",
+                                    isTextAlignCenter: true,
+                                  ),
+                                  CircularProgressIndicator(),
+                                ],
+                              ),
+                            );
+                          }
+
+                          if (controller.pokemonList.length == 0) {
+                            return Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.mood_bad_outlined),
+                                  DarkSmallTitle(
+                                    title: "Something went wrong...",
+                                    isTextAlignCenter: true,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: Text(
+                                      "Verify your connection and press \"Try again\" button...",
+                                    ),
+                                  ),
+                                  RaisedButton(
+                                    onPressed: controller.updatePokemonList,
+                                    child: Text("Try again"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+
+                          return Expanded(
+                            child: StaggeredGridView.extentBuilder(
+                              maxCrossAxisExtent: 180,
+                              mainAxisSpacing: 8,
+                              crossAxisSpacing: 8,
+                              itemBuilder: (_, index) {
+                                return PokemonTile(
+                                  backgroundColor: controller.getColorByType(
+                                      controller.pokemonList[index].type[0]),
+                                  pokemon: controller.pokemonList[index],
+                                );
+                              },
+                              staggeredTileBuilder: (i) => StaggeredTile.fit(1),
+                            ),
+                            // child: GridView.builder(
+                            //   itemCount: controller.pokemonList?.length ?? 0,
+                            //   gridDelegate:
+                            //       SliverGridDelegateWithMaxCrossAxisExtent(
+                            //     mainAxisSpacing: 8,
+                            //     crossAxisSpacing: 8,
+                            //     maxCrossAxisExtent: 180,
+                            //   ),
+                            //   itemBuilder:
+                            //   },
+                            // ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
