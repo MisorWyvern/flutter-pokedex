0d90e7178ae6844be3c0d7415165505e768b3457
MisorWyvern <gabriel_rosim@yahoo.com.br>
2021-02-26 17:37:31 -0300

    add onTap to pokemonTile, add lightSecondaryTitle widget

--- lib/core/widgets/pokemon_tile.dart
+++ lib/core/widgets/pokemon_tile.dart
@@ -6,81 +6,88 @@ import 'package:flutter_pokedex/features/pokemon_list/infra/dtos/pokemon_dto.dar
 class PokemonTile extends StatelessWidget {
   final PokemonDTO pokemon;
   final Color backgroundColor;
+  final Function onTap;
 
   const PokemonTile({
     Key key,
     this.pokemon,
     this.backgroundColor = Colors.grey,
+    this.onTap,
   })  : assert(pokemon != null),
         super(key: key);
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(16.0),
-      child: Container(
-        height: 132,
-        padding: EdgeInsets.only(left: 16, top: 16),
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          image: DecorationImage(
-            image: AssetImage("assets/pokeball20w.png"),
-            alignment: Alignment(1.2, 1.2),
-            fit: BoxFit.none,
-            scale: 2.25,
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              pokemon.name,
-              style: Theme.of(context).textTheme.headline6,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          height: 132,
+          padding: EdgeInsets.only(left: 16, top: 16),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            image: DecorationImage(
+              image: AssetImage("assets/pokeball20w.png"),
+              alignment: Alignment(1.2, 1.2),
+              fit: BoxFit.none,
+              scale: 2.25,
             ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      width: 96,
-                      child: CachedNetworkImage(
-                        imageUrl: "https://www.serebii.net/pokemon/art/" +
-                            pokemon.num +
-                            ".png",
-                        progressIndicatorBuilder: (_, __, downloadProgress) =>
-                            CircularProgressIndicator(
-                          value: downloadProgress.progress,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                pokemon.name,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        width: 96,
+                        child: CachedNetworkImage(
+                          imageUrl: "https://www.serebii.net/pokemon/art/" +
+                              pokemon.num +
+                              ".png",
+                          progressIndicatorBuilder: (_, __, downloadProgress) =>
+                              CircularProgressIndicator(
+                            value: downloadProgress.progress,
+                          ),
+                          errorWidget: (_, __, ___) => Icon(Icons.error),
                         ),
-                        errorWidget: (_, __, ___) => Icon(Icons.error),
                       ),
                     ),
-                  ),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemExtent: 24 + 16.0,
-                    itemCount: pokemon.type?.length ?? 0,
-                    shrinkWrap: true,
-                    itemBuilder: (_, i) {
-                      return Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Chip(
-                            backgroundColor: backgroundColor.withOpacity(0.6),
-                            label: Text(
-                              pokemon.type[i],
-                              style: TextStyle(
-                                fontSize: 12,
-                                color:
-                                    Theme.of(context).textTheme.headline6.color,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )),
-                      );
-                    },
-                  ),
-                ],
+                    ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      itemExtent: 24 + 16.0,
+                      itemCount: pokemon.type?.length ?? 0,
+                      shrinkWrap: true,
+                      itemBuilder: (_, i) {
+                        return Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Chip(
+                              backgroundColor: backgroundColor.withOpacity(0.6),
+                              label: Text(
+                                pokemon.type[i],
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .headline6
+                                      .color,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
